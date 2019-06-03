c9a1b580b634aa6fa2b5cd5370fdd99b8116495d
Caleb Powell <calebpowell@zipcoders-MacBook-Pro.local>
2019-06-03 12:56:23 -0400

    imported and revised is palindromic

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -5,4 +5,13 @@ public class Palindrome {
     public Integer countPalindromes(String input){
         return null;
     }
+
+    public static boolean isPalindromic(String str) {
+        StringBuilder x = new StringBuilder();
+        char[] arr = str.toCharArray();
+        for(char e : arr){
+            x.append(e);
+        }
+        return str.equals(x.reverse().toString());
+    }
 }
