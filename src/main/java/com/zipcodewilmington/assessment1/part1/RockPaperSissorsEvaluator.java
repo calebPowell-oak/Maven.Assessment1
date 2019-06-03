689e3928c136cf284930e2948e4bca5775b0f433
Caleb Powell <calebpowell@zipcoders-MacBook-Pro.local>
2019-06-03 10:24:00 -0400

    RockPaperScissorsEvaluator.java stub methods complete

--- src/main/java/com/zipcodewilmington/assessment1/part1/RockPaperSissorsEvaluator.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/RockPaperSissorsEvaluator.java
@@ -13,7 +13,17 @@ public class RockPaperSissorsEvaluator {
      * @return the respective winning move
      */
     public String getWinningMove(String handSign) {
-        return null;
+        switch (handSign){
+            case ROCK:{
+                return PAPER;
+            }
+            case PAPER:{
+                return SCISSOR;
+            }
+            default:{
+                return ROCK;
+            }
+        }
     }
 
     /**
@@ -21,7 +31,17 @@ public class RockPaperSissorsEvaluator {
      * @return the respective losing move
      */
     public String getLosingMove(String handSign) {
-        return null;
+        switch (handSign){
+            case ROCK:{
+                return SCISSOR;
+            }
+            case PAPER:{
+                return ROCK;
+            }
+            default:{
+                return PAPER;
+            }
+        }
     }
 
     /**
@@ -30,6 +50,10 @@ public class RockPaperSissorsEvaluator {
      * @return a string representative of the winning hand sign between the two players
      */
     public String getWinner(String handSignOfPlayer1, String handSignOfPlayer2) {
-        return null;
+        if(getLosingMove(handSignOfPlayer1).equals(handSignOfPlayer2)){
+            return handSignOfPlayer1;
+        } else {
+            return handSignOfPlayer2;
+        }
     }
 }
